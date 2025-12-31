b9776cb992e23614ec06b9918b9b1a920f3c5f89
Slimewizzard <slimeywizzard@gmail.com>
2025-12-31 17:00:47 +0100

    Update External.lua

--- External.lua
+++ External.lua
@@ -1,6 +1,9 @@
+-- !!Important !!
+-- when you drp your data from saved variable
+-- change the "Obsidian_config" to "Obsidian_config_external" 
 
 Obsidian_config_external = {
 	["flaggedPlayers"] = {
-		
+	
 	},
 }
